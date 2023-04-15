a2ea2e4e6cb229bbf69ddc536d0f82831e481d61
kouheigit <ookawakouhei777@gmail.com>
2023-04-16 00:29:03 +0900

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0.2f, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -0.2f, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // if (Input.GetKeyDown(KeyCode.LeftArrow))
+            
+            transform.Translate(-0.2f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+         //   if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(0.2f, 0, 0);
+        }
+    }
+}
