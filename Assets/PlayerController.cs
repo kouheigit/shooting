101b37ee6cdea2adf7e54006141db9c4e2de0c29
kouheigit <ookawakouhei777@gmail.com>
2023-04-20 17:32:05 +0900

    sound add2

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 60;    
     }
 
     // Update is called once per frame
@@ -33,7 +33,6 @@ public class PlayerController : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-     
         Destroy(gameObject);
     }
 }
