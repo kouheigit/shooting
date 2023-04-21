5881f0f4c9091b8ce02e1faf445a21e8789861a4
kouheigit <ookawakouhei777@gmail.com>
2023-04-21 14:13:01 +0900

    sound add8

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,10 +34,15 @@ public class PlayerController : MonoBehaviour
             transform.Translate(0.2f, 0, 0);
         }
     }
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
-        GetComponent<AudioSource>().Play();
         Destroy(gameObject);
     }
+    /*
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("miya");
+    }*/
 }
 
