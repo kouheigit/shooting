c6df32dfdb6ac44c9df7024e84c03fefc28c9253
kouheigit <ookawakouhei777@gmail.com>
2023-05-09 00:29:12 +0900

    enemy dev3

--- Assets/enemyController.cs
+++ Assets/enemyController.cs
@@ -31,8 +31,15 @@ public class enemyController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bullet")
         {
-            Destroy(gameObject);
+            ParticleSystem newParticle = Instantiate(particle);
+            // パーティクルの発生場所をこのスクリプトをアタッチしているGameObjectの場所にする。
+            newParticle.transform.position = this.transform.position;
+            // パーティクルを発生させる。
+            newParticle.Play();
+            // インスタンス化したパーティクルシステムのGameObjectを削除する。(任意)
+            // ※第一引数をnewParticleだけにするとコンポーネントしか削除されない。
+            Destroy(newParticle.gameObject, 5.0f);
         }
-
+        Destroy(gameObject);
     }
 }
